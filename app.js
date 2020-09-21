e05ddd85134cb98bc7ab17631d8599672d0f4b13
Lawrence <387820460@qq.com>
2020-09-22 02:57:16 +0800

    update cors

--- app.js
+++ app.js
@@ -2,18 +2,21 @@
 
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const tfnRouter = require('./src/router/tfnRouter');
 
 const port = process.env.PORT || 8000;
 
+app.use(cors());
+
 app.use('/', tfnRouter);
 
 app.use('/', (req, res) => {
-    // TO DO home page
-    return res.status(200).send(null);
+  // TO DO home page
+  return res.status(200).send(null);
 });
 
 app.listen(port, () => {
-    console.log(`Server start listening on Port ${port}`);
+  console.log(`Server start listening on Port ${port}`);
 });
